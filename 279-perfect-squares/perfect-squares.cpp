f0f3f19f4033db8e674dcc0232ff5ef84d0a588a
Akshay Kumar Dubey <123586645+akshaydubey05@users.noreply.github.com>
2024-02-08 12:32:22 +0530

    Time: 4 ms (98.08%) | Memory: 7.2 MB (92.48%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,16 +1,33 @@
-
-class Solution {
-public:
-    int numSquares(int n) {
-        vector<int> dp(n + 1, INT_MAX);
-        dp[0] = 0;
-        for (int i = 1; i <= n; ++i) {
-            for (int j = 1; j * j <= i; ++j){
-            dp[i] = min(dp[i], dp[i - j * j] + 1);
-            }
-        }
-        return dp[n];
+class Solution 
+{  
+private:  
+    int is_square(int n)
+    {  
+        int sqrt_n = (int)(sqrt(n));  
+        return (sqrt_n*sqrt_n == n);  
     }
-};
+    
+public:
+    int numSquares(int n) {  
+        if(is_square(n)) 
+        {
+            return 1;  
+        }
+    
+        while ((n & 3) == 0){
+            n >>= 2;  
+        }
+        if ((n & 7) == 7){
+            return 4;
+        }
 
-
+        int sqrt_n = (int)(sqrt(n)); 
+        for(int i = 1; i <= sqrt_n; i++){  
+            if (is_square(n - i*i)) {
+                return 2;  
+            }
+        }  
+        
+        return 3;  
+    }  
+}; 
