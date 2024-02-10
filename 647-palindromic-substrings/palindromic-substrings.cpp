df36b84f8d9d0cddad275dad2d78a2753ced0c1a
Akshay Kumar Dubey <123586645+akshaydubey05@users.noreply.github.com>
2024-02-10 12:37:59 +0530

    Time: 1280 ms (5.00%) | Memory: 599.8 MB (5.20%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -2,21 +2,12 @@ class Solution {
 public:
     bool isPalindrome(string S)
     {
-        // Stores the reverse of the
-        // string S
         string P = S;
-    
-        // Reverse the string P
         reverse(P.begin(), P.end());
-    
-        // If S is equal to P
         if (S == P) {
-            // Return "Yes"
             return true;
         }
-        // Otherwise
         else {
-            // return "No"
             return false;
         }
     }
