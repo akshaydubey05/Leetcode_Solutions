abb5eabdff304f1b8851cf1dd2b17c747ad8d7ac
Akshay Kumar Dubey <123586645+akshaydubey05@users.noreply.github.com>
2024-03-13 11:17:40 +0530

    Time: 104 ms (5.10%) | Memory: 7.3 MB (41.86%) - LeetSync

---
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int pivotInteger(int n) {
+        int leftsum=0;
+        int rightsum=0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                leftsum+=j;
+            }
+            for(int k=i;k<=n;k++){
+                rightsum+=k;
+            }
+            if(leftsum==rightsum){
+                return i;
+            }
+            else{
+                leftsum=0;
+                rightsum=0;
+            }
+            cout<<leftsum<<" "<<rightsum<<endl;
+        }
+        return -1;
+    }
+};
