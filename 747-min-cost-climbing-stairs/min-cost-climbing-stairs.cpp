4613f8e43246558a48b6174e5febf49e7c44e7e6
Akshay Kumar Dubey <123586645+akshaydubey05@users.noreply.github.com>
2024-06-27 13:01:49 +0530

    Time: 10 ms (14.88%) | Memory: 16.4 MB (39.73%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -10,12 +10,20 @@ public:
         dp[n]=cost[n]+min(solve(n-1,cost,dp),solve(n-2,cost,dp));
         return dp[n];
     }
+    int solve1(int n,vector<int> &cost,vector<int> &dp){
+        dp[0]=cost[0];
+        dp[1]=cost[1];
+        for(int i=2;i<=n;i++){
+            dp[i]=min(dp[i-1],dp[i-2])+cost[i];
+        }
+        return dp[n];
+    }
 
     int minCostClimbingStairs(vector<int>& cost) {
         int n=cost.size();
         cost.push_back(0);
         vector<int> dp(n+1,-1);
-        int x=solve(n,cost,dp);
+        int x=solve1(n,cost,dp);
         return x;
     }
 
