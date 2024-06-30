1462b52d0f7ae2a216fa2405cd16dc075609b370
Akshay Kumar Dubey <123586645+akshaydubey05@users.noreply.github.com>
2024-06-30 14:21:29 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (35.01%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
-    int rob(vector<int>& nums) {
-        int rob = 0;
-        int norob = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            int newRob = norob + nums[i];
-            int newNoRob = max(norob, rob);
-            rob = newRob;
-            norob = newNoRob;
+
+    int solve(vector<int>& nums,vector<int>& dp,int n){
+        if(n==0){
+            return nums[0];
         }
-        return max(rob, norob);
+        dp[0]=nums[0];
+        dp[1]=max(nums[0],nums[1]);
+        for(int i=2;i<nums.size();i++){
+            int inc=dp[i-2]+nums[i];
+            int exc=dp[i-1];
+            dp[i]=max(inc,exc);
+        }
+        return dp[n];
+    }
+
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> dp(n+1,-1);
+        int x=solve(nums,dp,n-1);
+        return x;
     }
 };
