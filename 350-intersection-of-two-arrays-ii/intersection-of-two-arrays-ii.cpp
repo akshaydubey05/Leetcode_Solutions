3ad4ac7c0c847e3a95d02d1be0b9e2fe22c7a0da
Akshay Kumar Dubey <123586645+akshaydubey05@users.noreply.github.com>
2024-07-02 14:35:44 +0530

    Time: 6 ms (32.80%) | Memory: 14.2 MB (15.24%) - LeetSync

---
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int,int>m1,m2;
+        for(int i=0;i<nums1.size();i++){
+            m1[nums1[i]]++;
+        }
+        for(int j=0;j<nums2.size();j++){
+            m2[nums2[j]]++;
+        }
+        vector<int> a;
+        for(auto i:m1){
+            for(auto j:m2){
+                if(i.first==j.first){
+                    int x=min(i.second,j.second);
+                    while(x--){
+                        a.push_back(i.first);
+                    }
+                }
+            }
+        }
+        return a;
+    }
+};
