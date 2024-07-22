2520c394ae590c8a783de8618d9f24f7eda88c85
Akshay Kumar Dubey <123586645+akshaydubey05@users.noreply.github.com>
2024-07-22 13:47:30 +0530

    Time: 26 ms (75.15%) | Memory: 27.4 MB (34.51%) - LeetSync

---
+++ 2502-sort-the-people/sort-the-people.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& n, vector<int>& h) {
+        vector<pair<int,string>> p;
+        for(int i=0;i<n.size();i++){
+            p.push_back(make_pair(h[i],n[i]));
+        }
+        sort(p.begin(),p.end());
+        reverse(p.begin(),p.end());
+        vector<string>s;
+        for(int i=0;i<p.size();i++){
+            s.push_back(p[i].second);
+        }
+        for(auto i: p){
+            cout<<i.first<<" "<<i.second;
+        }
+        return s;
+    }
+};
