98c3868a607b52d0a7cd5e2b47a864472ac857a8
Akshay Kumar Dubey <123586645+akshaydubey05@users.noreply.github.com>
2024-09-13 12:24:33 +0530

    Time: 11 ms (54.80%) | Memory: 30.8 MB (7.22%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -22,7 +22,22 @@ private:
 public:
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int> (amount + 1, -1));
-        return count_ways(n-1, amount, dp, coins);
+        vector<vector<long>> dp(n, vector<long> (amount + 1, 0));
+        for(int i = 0;i <= amount;i++){
+            if(i % coins[0] == 0){
+                dp[0][i] = 1;
+            }
+        }
+        for(int i = 1;i < n;i++){
+            for(int j = 0;j <= amount;j++){
+                long not_take = dp[i-1][j];
+                long take = 0;
+                if(coins[i] <= j){
+                    take = dp[i][j - coins[i]];
+                }
+                dp[i][j] = take + not_take;
+            }
+        }
+        return dp[n - 1][amount];
     }
 };
