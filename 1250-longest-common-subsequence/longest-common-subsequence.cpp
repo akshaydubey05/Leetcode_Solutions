06f253790219b69293aaea2f7fb8c1481c1fd5c0
Akshay Kumar Dubey <123586645+akshaydubey05@users.noreply.github.com>
2024-09-14 18:26:50 +0530

    Time: 46 ms (10.91%) | Memory: 26 MB (34.14%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,31 +1,22 @@
 class Solution {
-public:
-    // Function to find the length of the longest common subsequence in two strings.
-    int longestCommonSubsequence(string text1, string text2) {
-        int text1Length = text1.size(), text2Length = text2.size();
-        // Create a 2D array to store lengths of common subsequence at each index.
-        int dp[text1Length + 1][text2Length + 1];
-      
-        // Initialize the 2D array with zero.
-        memset(dp, 0, sizeof dp);
-      
-        // Loop through both strings and fill the dp array.
-        for (int i = 1; i <= text1Length; ++i) {
-            for (int j = 1; j <= text2Length; ++j) {
-                // If current characters match, add 1 to the length of the sequence
-                // until the previous character from both strings.
-                if (text1[i - 1] == text2[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                } else {
-                    // If current characters do not match, take the maximum length
-                    // achieved by either skipping the current character of text1 or text2.
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-                }
-            }
+private:
+    int longest_seq(int idx1, int idx2, vector<vector<int>>& dp, string& text1, string& text2){
+        if(idx1 < 0 || idx2 < 0){
+            return 0;
         }
-      
-        // Return the value in the bottom-right cell which contains the
-        // length of the longest common subsequence for the entire strings.
-        return dp[text1Length][text2Length];
+        if(dp[idx1][idx2] != -1){
+            return dp[idx1][idx2];
+        }
+        if(text1[idx1] == text2[idx2]){
+            return dp[idx1][idx2] = 1 + longest_seq(idx1-1, idx2-1, dp, text1, text2);
+        }
+        return dp[idx1][idx2] = max(longest_seq(idx1, idx2-1, dp, text1, text2), longest_seq(idx1-1, idx2, dp, text1, text2));
+    }
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.size();
+        int m = text2.size();
+        vector<vector<int>> dp(n, vector<int> (m+1, -1));
+        return longest_seq(n-1, m-1, dp, text1, text2);
     }
 };
