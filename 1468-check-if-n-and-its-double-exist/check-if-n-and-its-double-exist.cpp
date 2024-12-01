b31dc786635be64f445ec9d60b9cc404c7129cb6
Akshay Kumar Dubey <123586645+akshaydubey05@users.noreply.github.com>
2024-12-01 13:00:52 +0530

    Time: 10 ms (7.25%) | Memory: 13.5 MB (17.49%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -2,8 +2,9 @@ class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
         int ans = 0;
-        for(int i = 0;i < arr.size();i++){
-            for(int j = 0;j < arr.size();j++){
+        int n = arr.size();
+        for(int i = 0;i < n;i++){
+            for(int j = 0;j < n;j++){
                 if(i!=j){
                     if(arr[i] == 2*arr[j]){
                         ans++;
