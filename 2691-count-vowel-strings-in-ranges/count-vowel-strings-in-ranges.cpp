e7d9a06e393d2fd6cceb8da671908f431c41c91c
Akshay Kumar Dubey <123586645+akshaydubey05@users.noreply.github.com>
2025-01-02 14:17:28 +0530

    Time: 3 ms (89.32%) | Memory: 70.5 MB (46.26%) - LeetSync

--- 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.cpp
+++ 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
-    bool isVowel(char ch)
-    {
-        return (ch == 'a' || ch == 'e' || ch == 'i' || ch== 'o' || ch == 'u');
-    }
-
     vector<int> vowelStrings(vector<string>& words, vector<vector<int>>& queries) {
-        
+        vector<int> b;
+        set<char> s;
+        s.insert('a');s.insert('e');s.insert('i');s.insert('o');s.insert('u');
         int n = words.size();
-        int m = queries.size();
-
-        vector<int>prefix(n,0);
-        vector<int>result(m,0);
-
-        for(int i=0;i<words.size();i++)
-        {
-            int isVowelAtEnds = isVowel(words[i][0]) && isVowel(words[i][words[i].size()-1] );
-            prefix[i] += (i==0)?isVowelAtEnds: isVowelAtEnds+prefix[i-1];
+        for(int i=0;i<n;i++){
+            string st = words[i];
+            if(s.find(st[0])!=s.end() && s.find(st[st.length()-1]) != s.end()){
+                b.push_back(1);
+            }
+            else{
+                b.push_back(0);
+            }
         }
-        
-        for(int i=0;i<queries.size();i++)
-        {
-            int start = queries[i][0];
-            int end = queries[i][1];
-
-            result[i] =(start==0)? prefix[end] : prefix[end] - prefix[start-1];
+        vector<int> prefix(n);
+        prefix[0] = b[0];
+        for(int i=1;i<n;i++){
+            prefix[i] = prefix[i-1] + b[i];
         }
-
-        return result;
+        vector<int> ans(queries.size());
+        for(int i=0;i<queries.size();i++){
+            int x = queries[i][0];
+            if(x>0){
+                x=prefix[queries[i][0]-1];
+            }
+            else{
+                x=0;
+            }
+            ans[i] = prefix[queries[i][1]]-x;
+        }
+        return ans;
     }
 };
