865667757efd67510cf020d7c5f40e209172fa61
Akshay Kumar Dubey <123586645+akshaydubey05@users.noreply.github.com>
2025-07-05 20:43:22 +0530

    Time: 0 ms (100.00%) | Memory: 23.6 MB (57.56%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,18 +1,44 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int,int> mp;
-        for(auto i: nums){
-            mp[i]++;
-        }
-        int ans=0;
-        vector<int> aa;
-        for(auto i: mp){
-            if(i.second>(nums.size()/3)){
-                ans++;
-                aa.push_back(i.first);
+        int c1=0, c2=0, e1=INT_MIN, e2=INT_MIN;
+        for(int i=0;i<nums.size();i++){
+            if(c1 == 0 && e2!=nums[i]){
+                c1++;
+                e1=nums[i];
+            }
+            else if(c2 == 0 && e1!=nums[i]){
+                c2++;
+                e2=nums[i];
+            }
+            else if(e1 == nums[i]){
+                c1++;
+            }
+            else if(e2 == nums[i]){
+                c2++;
+            }
+            else{
+                c1--;
+                c2--;
             }
         }
-        return aa;
+        c1=0,c2=0;
+            vector<int> ss;
+            for(int i=0;i<nums.size();i++){
+                if(nums[i] == e1){
+                    c1++;
+                }
+                if(nums[i] == e2){
+                    c2++;
+                }
+            }
+            vector<int> ans;
+            if(c1 >(nums.size()/3)){
+                ans.push_back(e1);
+            }
+            if(c2 >(nums.size()/3)){
+                ans.push_back(e2);
+            }
+            return ans;
     }
 };
