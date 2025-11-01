69c38e8139204cfdde1bd3ee2ff06f27aa11237b
Akshay Kumar Dubey <123586645+akshaydubey05@users.noreply.github.com>
2025-11-01 13:42:40 +0530

    Time: 3 ms (54.89%) | Memory: 32.5 MB (97.24%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,20 +1,33 @@
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        int cnt=0;
-        int l=0;
-        int sum=0;
-        int ans=0;
-        unordered_map<int,int> pre;
-        pre[0]=1;
-        for(int i=0;i<nums.size();i++){
-            sum+=nums[i];
-            if (pre.count(sum - goal)) {
-                ans += pre[sum - goal];
+        int cnt1 = 0, cnt2 = 0;
+        int l1 = 0, l2 = 0;
+        int ans1 = 0, ans2 = 0;
+
+        // count subarrays with sum <= goal
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 1) cnt1++;
+            while (cnt1 > goal) {
+                if (nums[l1] == 1) cnt1--;
+                l1++;
             }
-            pre[sum]++;
+            ans1 += (i - l1 + 1);
         }
-        cout<<sum<<endl;
-        return ans;
+
+        // if goal == 0 then atMost(goal - 1) = atMost(-1) = 0
+        if (goal == 0) return ans1;
+
+        // count subarrays with sum <= goal - 1
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 1) cnt2++;
+            while (cnt2 > goal - 1) {
+                if (nums[l2] == 1) cnt2--;
+                l2++;
+            }
+            ans2 += (i - l2 + 1);
+        }
+
+        return ans1 - ans2;
     }
 };
